6ffb5046111db2d09649c1457352061c72a66cdc
Luchs <Luchs@euirc.eu>
2013-02-24 23:25:49 +0100

    run.cmd: Use global CoffeeScript

--- run.cmd
+++ run.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-call "cmd /c start supervisor --exec .\node_modules\.bin\coffee.cmd app.coffee"
+call "cmd /c start supervisor --exec coffee.cmd app.coffee"
 call "cmd /c start grunt watch"
