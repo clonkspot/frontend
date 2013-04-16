ad268df820e0e76ad847f28b243b3031f9b86c62
Luchs <Luchs@euirc.eu>
2013-04-16 22:31:52 +0200

    Cache the Twitch request for 5min

--- js/twitch.coffee
+++ js/twitch.coffee
@@ -2,11 +2,24 @@
 
 CHANNEL_URL = 'https://api.twitch.tv/kraken/streams/Nachtfalter89?callback=?'
 
-# JSONP request
-$.getJSON(CHANNEL_URL).then (data) ->
-  # Check whether there's a stream running.
+# Check whether there's a stream running.
+checkStream = (data) ->
   if data?.stream?.game?.match /clonk/i
     $('header .twitch')
       .slideDown()
       .find('a')
         .attr('href', data.stream.channel.url)
+
+lastCheck = sessionStorage['twitch.time']
+prev = sessionStorage['twitch.stream']
+
+# Only check every 5min.
+if prev and lastCheck and Date.now() - lastCheck < 3e5
+  checkStream(JSON.parse prev)
+else
+  # JSONP request
+  $.getJSON(CHANNEL_URL).then (data) ->
+    # Cache the result.
+    sessionStorage['twitch.time'] = Date.now()
+    sessionStorage['twitch.stream'] = JSON.stringify data
+    checkStream data
--- public/js/main.js
+++ public/js/main.js
@@ -433,16 +433,30 @@ process.binding = function (name) {
 });
 
 require.define("/twitch.coffee",function(require,module,exports,__dirname,__filename,process,global){(function() {
-  var CHANNEL_URL;
+  var CHANNEL_URL, checkStream, lastCheck, prev;
 
   CHANNEL_URL = 'https://api.twitch.tv/kraken/streams/Nachtfalter89?callback=?';
 
-  $.getJSON(CHANNEL_URL).then(function(data) {
+  checkStream = function(data) {
     var _ref, _ref1;
     if (data != null ? (_ref = data.stream) != null ? (_ref1 = _ref.game) != null ? _ref1.match(/clonk/i) : void 0 : void 0 : void 0) {
       return $('header .twitch').slideDown().find('a').attr('href', data.stream.channel.url);
     }
-  });
+  };
+
+  lastCheck = sessionStorage['twitch.time'];
+
+  prev = sessionStorage['twitch.stream'];
+
+  if (prev && lastCheck && Date.now() - lastCheck < 3e5) {
+    checkStream(JSON.parse(prev));
+  } else {
+    $.getJSON(CHANNEL_URL).then(function(data) {
+      sessionStorage['twitch.time'] = Date.now();
+      sessionStorage['twitch.stream'] = JSON.stringify(data);
+      return checkStream(data);
+    });
+  }
 
 }).call(this);
 
