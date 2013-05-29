d2f31dafaf28b65f1e197449be69e5a54199ffe3
Luchs <Luchs@euirc.eu>
2013-05-29 22:55:45 +0200

    Implement blog import via YQL

--- js/news.js
+++ js/news.js
@@ -131,10 +131,43 @@ angular.module('clonkspotNewsApp', [])
     }
     return ImportYouTube
   }])
-  .factory('ImportSites', ['ImportYouTube', 'language', function(ImportYouTube, language) {
+  .factory('ImportAtom', ['$http', 'language', function($http, language) {
+    function ImportAtom(name, type, feed) {
+      this.name = name
+      this.type = type
+      this.feed = feed
+      this.items = []
+    }
+
+    ImportAtom.prototype.name = 'Atom Feed'
+    ImportAtom.prototype.type = 'newpost'
+    ImportAtom.prototype.url = function() {
+      return "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'" + encodeURIComponent(this.feed) + "'%20and%20itemPath%3D'feed.entry'&format=json&callback=JSON_CALLBACK"
+    }
+    ImportAtom.prototype.getItems = function() {
+      var self = this
+      $http.jsonp(this.url())
+        .success(function(feed) {
+          // Transform videos.
+          self.items = feed.query.results.entry.map(function(entry) {
+            return {
+              title: entry.title.content,
+              author: entry.author.name,
+              link: entry.link[0].href,
+              date: entry.published.slice(0, 10),
+              type: self.type,
+              lang: language
+            }
+          })
+        })
+    }
+    return ImportAtom
+  }])
+  .factory('ImportSites', ['ImportYouTube', 'ImportAtom', 'language', function(ImportYouTube, ImportAtom, language) {
     var youtubeList = language == 'de' ? 'PLigNApmAXiiRp69Gw_2U1MN1vhiYLdkQH' : 'PLigNApmAXiiTBM7vXR0hwyBV2o61lqj0S'
     return {
-      youtube: new ImportYouTube(youtubeList)
+      youtube: new ImportYouTube(youtubeList),
+      blog: new ImportAtom('Blog', 'blog', 'http://clonkspot.org/blog/feed/atom/')
     }
   }])
   .controller('ImportCtrl', ['$scope', 'ImportSites', function($scope, ImportSites) {
